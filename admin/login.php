835bcfdb7d7aa9bb8a97c235353bd468830a1bc7
hyoukhoon <hyoukhoon@gmail.com>
2022-02-24 17:49:08 +0900

    ss

--- admin/login.php
+++ admin/login.php
@@ -4,16 +4,16 @@ include $_SERVER["DOCUMENT_ROOT"]."/inc/header.php";
 <body class="text-center">
     
         <main class="form-signin">
-        <form style="padding:200px;">
+        <form style="padding:200px;" method="post" action="login_ok.php">
             <img class="mb-4" src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
             <h1 class="h3 mb-3 fw-normal">쇼핑몰 관리자 페이지</h1>
 
             <div class="form-floating" style="margin-bottom:10px;">
-            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <input type="text" name="userid" class="form-control" id="floatingInput" placeholder="name@example.com" required>
             <label for="floatingInput">아이디를 입력하세요</label>
             </div>
             <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <input type="password" name="passwd" class="form-control" id="floatingPassword" placeholder="Password" required>
             <label for="floatingPassword">비밀번호를 입력하세요.</label>
             </div>
 
---
+++ admin/login_ok.php
@@ -0,0 +1,29 @@
+<?php session_start();
+include $_SERVER["DOCUMENT_ROOT"]."/inc/dbcon.php";
+ini_set( 'display_errors', '0' );
+
+$userid=$_POST["userid"];
+$passwd=$_POST["passwd"];
+$passwd=hash('sha512',$passwd);
+
+$query = "select * from admins where userid='".$userid."' and passwd='".$passwd."' and end_login_date>now()";
+$result = $mysqli->query($query) or die("query error => ".$mysqli->error);
+$rs = $result->fetch_object();
+
+if($rs){
+    $sql="update admins set last_login=now() where idx=".$rs->idx;
+    $result=$mysqli->query($sql) or die($mysqli->error);
+    $_SESSION['AUID']= $rs->userid;
+    $_SESSION['AUNAME']= $rs->username;
+    $_SESSION['ALEVEL']= $rs->level;
+
+    echo "<script>alert('어서오십시오.');location.href='/admin/admin_list.php';</script>";
+    exit;
+
+}else{
+    echo "<script>alert('아이디나 암호가 틀렸습니다. 다시한번 확인해주십시오.');history.back();</script>";
+    exit;
+}
+
+
+?>
