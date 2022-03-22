51288249ed4e804a39fcb4c39b74270fcd0dd046
hyoukhoon <hyoukhoon@gmail.com>
2022-03-22 17:27:17 +0900

    aa

--- admin/product/option_change.php
+++ admin/product/option_change.php
@@ -4,10 +4,13 @@ ini_set( 'display_errors', '0' );
 
 $poid = $_POST['poid'];
 
-$query="select image_url from product_options where poid='".$poid."'";
+$query="select * from product_options where poid='".$poid."'";
 $result = $mysqli->query($query) or die("query error => ".$mysqli->error);
 $rs = $result->fetch_object();
-$html = $rs->image_url;
-echo $html;
+$image_url = $rs->image_url;
+$option_price = $rs->option_price;
+
+$data = array("image_url"=>$image_url,"option_price"=>$option_price);
+echo json_encode($data);
 
 ?>
--- admin/product/product_view.php
+++ admin/product/product_view.php
@@ -23,6 +23,20 @@ while($rs2 = $result2->fetch_object()){
     .col{
         border: 1px solid #f1f1f1;
     }
+
+    [type=radio] { 
+        position: absolute;
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
+
+    [type=radio] + span {
+        cursor: pointer;
+    }
+    [type=radio]:checked + span {
+        outline: 5px solid indigo;
+    }
 </style>
 <link
   rel="stylesheet"
@@ -38,9 +52,10 @@ while($rs2 = $result2->fetch_object()){
     <div class="col">
       <h3><?php echo $rs->name;?></h3>
         <div>
-            가격 : <?php echo number_format($rs->price);?>원
+            가격 : <span id="price"><?php echo number_format($rs->price);?></span>원
         </div>
         <?php if($options){?>
+        <br>
         <div>
             옵션 : 
             <select name="poption" id="poption">
@@ -49,6 +64,16 @@ while($rs2 = $result2->fetch_object()){
                     <option value="<?php echo $op->poid;?>"><?php echo $op->option_name;?></option>
                 <?php }?>
             </select>
+        </div>
+        <br>
+        <div>
+
+            <?php foreach($options as $op){?>
+                <input type="radio" name="poption" class="optradio" id="poption_<?php echo $op->poid;?>" value="<?php echo $op->poid;?>">
+                    <span  onclick="jQuery('#poption_<?php echo $op->poid;?>').click();" style="content:url(<?php echo $op->image_url;?>);height:100px;width:100px;"></span>
+                </input>
+            <?php }?>
+            
         </div>
         <?php }?>
     </div>
@@ -61,6 +86,31 @@ while($rs2 = $result2->fetch_object()){
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script>
+    $("input[name='poption']:radio").change(function () {
+        var poid = $('input:radio[name="poption"]:checked').val();
+        var data = {
+            poid : poid
+        };
+            $.ajax({
+                async : false ,
+                type : 'post' ,
+                url : 'option_change.php' ,
+                data  : data ,
+                dataType : 'json' ,
+                error : function() {} ,
+                success : function(data) {
+                    var price=parseInt(data.option_price)+<?php echo $rs->price;?>;
+                    $("#pimg").attr("src", data.image_url);
+                    $("#price").text(number_format(price));
+                }
+        });
+    });
+
+    function number_format(num){ 
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g,','); 
+    }
+
+
     $("#poption").change(function(){
         var poid = $("#poption option:selected").val();
         
